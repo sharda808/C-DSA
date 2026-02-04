1cd279e219534d4f82b6795003ad80fe43b452cc
sharda <shardakumari96115@gmail.com>
2026-02-05 00:40:57 +0530

    updated graph code

---
+++ Graph/BFSTraversal.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+void buildGraph(int n, vector<vector<int>>edges, vector<int>adj[]){
+  for(auto e: edges){
+    int u= e[0];
+    int v = e[1];
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+}
+void bfs(int start, int n, vector<int>adj[]){
+  vector<bool>vis(n,false);
+queue<int>q;
+vis[start] = true;
+q.push(start);
+while(!q.empty()){
+  int node = q.front();
+  q.pop();
+  cout<<node<<" ";
+  for(int neigh:adj[node]){
+    if(!vis[neigh]){
+      vis[neigh] = true;
+      q.push(neigh);
+    }
+  }
+}
+}
+int main(){
+    int  n= 5 ;// no of nodes/vertices
+  vector<vector<int>> edges = {{0,1},{0,2},{1,3},{3,4}};
+  vector<int>adj[n]; 
+  buildGraph(n,edges, adj);
+  bfs(0,n,adj);
+  return 0;
+}
--- Graph/Buildgraph.cpp
+++ Graph/Buildgraph.cpp
@@ -1,36 +1,28 @@
 #include<iostream>
 #include<vector>
-#include<list>
 using namespace std;
-class Graph {
-  int v;
-  list<int> *l;
-  public:
-  Graph(int v){
-this->v = v;
-l = new list<int>[v];
+void buildGraph(int n, vector<vector<int>>&edges, vector<int>adj[]){
+  for(auto e:edges){
+    int u = e[0];
+    int v= e[1];
+    adj[u].push_back(v);
+adj[v].push_back(u);
   }
-  void addEdge(int u, int v){
-    l[u].push_back(v);
-    l[v].push_back(u);
-  }
-  void printAdjList(){
-    for(int i=0; i<v; i++){
-      cout<<i<<" -> ";
-      for(int neigh:l[i]){
-        cout<<neigh<<" ";
-      }
-      cout<<endl;
+}
+void printGraph(int n, vector<int>adj[]){
+  for(int i=0; i<n;i++){
+    cout<<i<<" -> ";
+    for(int x:adj[i]){
+      cout<<x<<" ";
     }
+    cout<<endl;
   }
-};
+}
 int main(){
-  Graph g(5);
-g.addEdge(0,1);
-g.addEdge(1,2);
-g.addEdge(1,3);
-g.addEdge(2,3);
-g.addEdge(2,4);
-g.printAdjList();
-return 0;
+  int  n= 5 ;// no of nodes/vertices
+  vector<vector<int>> edges = {{0,1},{0,2},{1,3},{3,4}};
+  vector<int>adj[n];
+  buildGraph(n, edges,adj);
+  printGraph(n,adj);
+  return 0;
 }
